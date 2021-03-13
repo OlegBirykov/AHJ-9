998f4cf1ad1913a6d07026c483507570e4780c26
OlegBirykov <p_petro75@mail.ru>
2021-03-13 23:19:55 +0300

    Задание №1 выполнено

--- src/js/app.js
+++ src/js/app.js
@@ -1,2 +1,22 @@
-// eslint-disable-next-line no-console
-console.log('it works!');
+import CollapseWidget from './task1/CollapseWidget';
+
+const collapseWidget = new CollapseWidget(
+  document.getElementById('container1'),
+  'Collapse',
+  'Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident.',
+);
+collapseWidget.bindToDOM();
+
+const collapseWidget2 = new CollapseWidget(
+  document.getElementById('container2'),
+  'Collapse, ура!!!',
+  'Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident.',
+);
+collapseWidget2.bindToDOM();
+
+const collapseWidget3 = new CollapseWidget(
+  document.getElementById('container3'),
+  'Collapse, ура!!!',
+  '',
+);
+collapseWidget3.bindToDOM();
